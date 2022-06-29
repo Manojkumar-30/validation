782980f489c09c8be10698adadf31c7c2c13d47d
Manoj Kumar <94695167+Mkvelan@users.noreply.github.com>
2022-06-28 22:43:31 -0700

    update

--- login.js
+++ login.js
@@ -1,63 +1,101 @@
-<script>
-            function GEEKFORGEEKS() {
-                var name =
-                    document.forms.RegForm.Name.value;
-                var email =
-                    document.forms.RegForm.EMail.value;
-                var phone =
-                    document.forms.RegForm.Telephone.value;
-                var what =
-                    document.forms.RegForm.Subject.value;
-                var password =
-                    document.forms.RegForm.Password.value;
-                var address =
-                    document.forms.RegForm.Address.value;
-                var regEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;  //Javascript reGex for Email Validation.
-                var regPhone=/^\d{10}$/;                                        // Javascript reGex for Phone Number validation.
-                var regName = /\d+$/g;                                    // Javascript reGex for Name validation
- 
-                if (name == "" || regName.test(name)) {
-                    window.alert("Please enter your name properly.");
-                    name.focus();
-                    return false;
-                }
- 
-                if (address == "") {
-                    window.alert("Please enter your address.");
-                    address.focus();
-                    return false;
-                }
-                 
-                if (email == "" || !regEmail.test(email)) {
-                    window.alert("Please enter a valid e-mail address.");
-                    email.focus();
-                    return false;
-                }
-                  
-                if (password == "") {
-                    alert("Please enter your password");
-                    password.focus();
-                    return false;
-                }
- 
-                if(password.length <6){
-                    alert("Password should be atleast 6 character long");
-                    password.focus();
-                    return false;
- 
-                }
-                if (phone == "" || !regPhone.test(phone)) {
-                    alert("Please enter valid phone number.");
-                    phone.focus();
-                    return false;
-                }
- 
-                if (what.selectedIndex == -1) {
-                    alert("Please enter your course.");
-                    what.focus();
-                    return false;
-                }
- 
-                return true;
-            }
-        </script>
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const phoneValue = phone.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	if(phoneValue ===''){
+
+		setErrorFor(phone,'Phone number cannot be blank');
+	}
+	else if (phoneValue.length < 10  ) {
+
+		setErrorFor(phone, 'Not valid');
+	}
+	else{
+		setSuccessFor(phone);
+	}
+	var phone_input = document.getElementById("phone");
+
+	phone_input.addEventListener('input', () => {
+	phone_input.setCustomValidity('');
+	phone_input.checkValidity();
+	});
+
+	phone_input.addEventListener('invalid', () => {
+	if(phone_input.value === '') {
+		phone_input.setCustomValidity('Enter phone number!');
+		setErrorFor(phone, 'Not valid');
+	} else {
+		phone_input.setCustomValidity('Enter phone number in this format: 123-456-7890');
+	}
+	});
+
+    if(passwordValue === '') {
+        setErrorFor(password, 'Password is required');
+    }
+     else if (passwordValue.length < 8 ) {
+        setErrorFor(password, 'Password must be at least 8 character.');
+    } 
+    else {
+        setSuccessFor(password);
+    }
+	if (passwordValue.match(/[!\@\#\$\%\^\&\*\(\)\_\-\+\=\<\>\.\,]/)){
+        setErrorFor(password, 'Password must contain lowercase, Uppercase and special characters');
+    }
+
+    if(password2Value === '') {
+        setErrorFor(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setErrorFor(password2, "Passwords doesn't match");
+    } else {
+        setSuccessFor(password2);
+    }
+
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+
